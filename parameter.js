e2cc90396696aaf4ff3ffc9c9332d51729ba772c
islamajabul <ajabulmd1@gmail.com>
2023-01-31 15:47:02 +0800

    first commit

---
+++ parameter.js
@@ -0,0 +1,26 @@
+// function bringSingara(money){
+//     console.log('mama singara den');
+//     console.log(money);
+// }
+// bringSingara(250);
+
+// function bringSingara(money){
+//     console.log('ai nen singara');
+//     console.log(money);
+// }
+// var taka = 300;
+// bringSingara(taka);
+
+function add(num1,num2){
+    console.log('going to add', num1,num2);
+
+}
+
+// add(50,75);
+
+function sum(a,b,c,d,e){
+    console.log(a,b,c,d,e);
+    var sum = a+b+c+d+e;
+    console.log(sum);
+}
+sum(10,20,30,55,88);
