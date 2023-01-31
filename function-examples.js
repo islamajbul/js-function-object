bd837850374d9fbf2d770771967dcd7b5335832f
islamajabul <ajabulmd1@gmail.com>
2023-01-31 16:57:02 +0800

    Function Example

---
+++ function-examples.js
@@ -0,0 +1,20 @@
+/* function functionName(parameters){
+    function body
+    return
+
+}
+
+var returnValue = functionName(parameters value) */
+
+function getAverage(assignment1, assignment2, assignment3){
+    const total = assignment1 + assignment2 + assignment3;
+    const average = total / 3;
+    return average;
+}
+
+const assignment1Marks = 55;
+const assignment2Marks = 58;
+const assignment3Marks = 57;
+
+var myAverage = getAverage(assignment1Marks, assignment2Marks, assignment3Marks);
+console.log('My average so far:',myAverage);
