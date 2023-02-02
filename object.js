afb523dc046d8e88a43e9c4c69850d36882ec6bf
islamajabul <ajabulmd1@gmail.com>
2023-02-02 23:52:47 +0800

    object

---
+++ object.js
@@ -0,0 +1,24 @@
+/* var student = { id: 115, 
+    name: 'Ajabul Islam'
+    class: 10,
+    marks: 98;
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '64gb',
+    camera: '7MP';
+}
+ */
+var myComputer = {
+    brand: 'lenovo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7'
+
+}
+
+console.log(myComputer);
+myComputer.processor = 'i79';
+console.log(myComputer)
