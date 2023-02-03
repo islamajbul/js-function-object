d9f38ad589204d37862b41846d76fc552f126cae
islamajabul <ajabulmd1@gmail.com>
2023-02-03 23:23:32 +0800

    switch

---
+++ switch.js
@@ -0,0 +1,40 @@
+const color = 'dot';
+/* if(color === 'green'){
+    console.log('It is green color');
+}
+else if (color === 'blue'){
+    console.log('It is blue color');
+}
+else if (color === 'red'){
+    console.log('It is red color');
+}
+else if (color === 'white'){
+    console.log('It is white color');
+}
+else if (color === 'yellow'){
+    console.log('It is yellow color');
+}
+else{
+    console.log('it is black color');
+} */
+
+// switch
+switch(color){
+    case 'green':
+        console.log('it is green color');
+        break;
+    case 'blue':
+        console.log('it is blue color');
+        break;
+    case 'white':
+        console.log('it is white color');
+        break;
+    case 'red':
+        console.log('it is red color');
+        break;
+    case 'yellow':
+        console.log('it is yellow color');
+        break;
+    default:
+        console.log('it is black color');
+}
